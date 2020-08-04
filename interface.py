ef5c1d150a659deca20ea72e8fc01d2dd0eb83d3
zn <nanzheng03@gmail.com>
2020-08-04 20:44:44 +0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -28,20 +28,24 @@ class ConsoleInterface:
 class TextInterface:
     def __init__(self):
         stdscr = curses.initscr()
-        self.boardwin = curses.newwin(11, 18, 1, 1)
-        self.msgwin = curses.newwin(5, 40, 11, 1)
-        self.inputwin = curses.newwin(3, 40, 16, 1)
+        self.boardwin = curses.newwin(11, 21, 1, curses.COLS // 2 - (21 // 2))
+        self.msgwin = curses.newwin(5, 40, 12, curses.COLS // 2 - (40 // 2))
+        self.inputwin = curses.newwin(3, 30, 18, curses.COLS // 2 - (30 // 2))
+
 
     def set_board(self, inputstr):
-        self.boardwin.addstr(0, 0, inputstr)
+        self.boardwin.addstr(1, 2, inputstr)
+        self.boardwin.box()
         self.boardwin.refresh()
 
     def set_msg(self, inputstr):
-        self.msgwin.addstr(0, 0, inputstr)
+        self.msgwin.addstr(0, 1,inputstr)
+        self.msgwin.box()
         self.msgwin.refresh()
 
     def get_player_input(self, msg):
-        self.inputwin.addstr(0, 0, msg)
+        self.inputwin.addstr(1, 1, msg)
+        self.inputwin.box()
         self.inputwin.refresh()
         value = self.inputwin.getstr().decode("utf-8")
         self.inputwin.erase()
